8d242a8b7d755de65e60adde43f6281788305e08
SCDOM\OAKA <oaka@simcorp.com>
2022-11-21 17:33:36 +0100

    Revert "introduce an error commit"
    This reverts commit d097fd7b181dad96cad21641448774aeb774e714.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,6 +6,7 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
             Console.WriteLine("Feature branch 1 - development 1");
         }
     }
