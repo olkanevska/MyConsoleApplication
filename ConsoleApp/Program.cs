e8c4cdb27a0ffa8553bfc807b4a18c87e401ae04
SCDOM\OAKA <oaka@simcorp.com>
2022-11-21 17:14:41 +0100

    Changed console message

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Feature branch 2 - development 1");
         }
     }
 }
